86312b18fd5edf5d51290b497f0546fbd918e994
muhammadsahil14725 <59023207+muhammadsahil14725@users.noreply.github.com>
2021-10-25 23:04:05 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,28 +1,8 @@
-// In App.js in a new project
-
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {LoginScreen} from './components/screens/LoginScreen'
+export default class App extends React.Component {
+  render() {
+    return <LoginScreen />;
+  }
 }
-
-const Stack = createNativeStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-export default App;
